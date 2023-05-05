8e7f3e6a04f43e72df1b44304818000e3974decf
arieltlm <695353064@qq.com>
2023-05-05 17:43:10 +0800

    add vue2-elementui-test

---
+++ vue2-elementui-test/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<script>
+import SearchBox from '../components/SearchBox.vue'
+import TableBox from '../components/TableBox.vue'
+import Count from '../components/Count.vue'
+export default {
+  components: {SearchBox, TableBox, Count},
+}
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <SearchBox></SearchBox>
+    <Count></Count>
+    <hr />
+    <TableBox></TableBox>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+</style>
